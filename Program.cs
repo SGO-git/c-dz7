34fe90fcc0a20ce10b9276b3530ad78a79da6323
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-13 00:32:35 +0500

    add dz7

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+System.Console.WriteLine("Введите число дня недели");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number > 0 && Number < 8)
+{
+    if (Number == 6 || Number == 7)
+    {
+        System.Console.WriteLine("yes");
+    }
+    else
+    {
+        System.Console.WriteLine("No");
+    }
+}
+else
+{
+    System.Console.WriteLine("Wrong number");
+}
